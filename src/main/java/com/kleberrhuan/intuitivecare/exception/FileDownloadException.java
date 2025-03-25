2ffb29da6cfc166f118b23bdc7b6f920a04a1e71
KleberRhuan <kleber_rhuan@hotmail.com>
2025-03-25 20:18:49 -0300

    feat: Created Exception for IO operations errors

---
+++ src/main/java/com/kleberrhuan/intuitivecare/exception/FileDownloadException.java
@@ -0,0 +1,7 @@
+package com.kleberrhuan.intuitivecare.exception;
+
+public class FileDownloadException extends RuntimeException {
+    public FileDownloadException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
---
+++ src/main/java/com/kleberrhuan/intuitivecare/exception/WebsiteConnectionException.java
@@ -0,0 +1,7 @@
+package com.kleberrhuan.intuitivecare.exception;
+
+public class WebsiteConnectionException extends RuntimeException {
+    public WebsiteConnectionException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
