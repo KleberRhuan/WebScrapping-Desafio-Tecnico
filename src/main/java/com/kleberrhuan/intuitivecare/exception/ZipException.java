06a70075204557bb64adfef251fdfe86ba60aa92
KleberRhuan <kleber_rhuan@hotmail.com>
2025-03-26 00:30:07 -0300

    feat: created zip archiver

---
+++ src/main/java/com/kleberrhuan/intuitivecare/exception/ZipException.java
@@ -0,0 +1,7 @@
+package com.kleberrhuan.intuitivecare.exception;
+
+public class ZipException extends RuntimeException {
+    public ZipException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
