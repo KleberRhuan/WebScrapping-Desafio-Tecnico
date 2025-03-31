bae1f14177aaf66c53db68fec1ec5429bca0d2e9
KleberRhuan <kleber_rhuan@hotmail.com>
2025-03-31 04:44:42 -0300

    chore: Improved Sql querys

--- sql/3.2/01_schema.sql
+++ sql/3.2/01_schema.sql
@@ -1,29 +1,33 @@
 -- Script para criação de schemas e tabelas
 create schema if not exists config;
 create schema if not exists cadop;
-create schema if not exists accounting;
+CREATE COLLATION IF NOT EXISTS pt_br_ci_ai (
+    PROVIDER = icu,
+    DETERMINISTIC = FALSE,
+    LOCALE = 'pt-u-ks-level1'
+    );
 
 -- Tabela criada com Base no dicionario de dados presente no link https://dadosabertos.ans.gov.br/FTP/PDA/operadoras_de_plano_de_saude_ativas/
 CREATE TABLE IF NOT EXISTS cadop.cadastro_operadoras (
      id bigserial PRIMARY KEY,
      registro_operadora       varchar(6),  -- Registro da operadora (6 dígitos)
      cnpj                     varchar(14),  -- CNPJ da Operadora
-     razao_social             varchar(140),  -- Razão Social da Operadora
-     nome_fantasia            varchar(140),  -- Nome Fantasia da Operadora
-     modalidade               varchar(100),  -- Modalidade (classificação conforme estatuto jurídico)
-     logradouro               varchar(40),  -- Endereço da Sede da Operadora
-     numero                   varchar(20),  -- Número do endereço
-     complemento              varchar(40),  -- Complemento do endereço
-     bairro                   varchar(30),  -- Bairro
-     cidade                   varchar(30),  -- Cidade
+     razao_social             varchar(140) collate "pt_br_ci_ai" ,  -- Razão Social da Operadora
+     nome_fantasia            varchar(140) collate "pt_br_ci_ai" ,  -- Nome Fantasia da Operadora
+     modalidade               varchar(100) collate "pt_br_ci_ai" ,  -- Modalidade (classificação conforme estatuto jurídico)
+     logradouro               varchar(40) collate "pt_br_ci_ai" ,  -- Endereço da Sede da Operadora
+     numero                   varchar(20) collate "pt_br_ci_ai" ,  -- Número do endereço
+     complemento              varchar(40) collate "pt_br_ci_ai" ,  -- Complemento do endereço
+     bairro                   varchar(30) collate "pt_br_ci_ai" ,  -- Bairro
+     cidade                   varchar(30) collate "pt_br_ci_ai" ,  -- Cidade
      uf                       char(2),  -- Estado (UF)
      cep                      char(8),  -- CEP
      ddd                      varchar(4),  -- Código de DDD da Operadora
      telefone                 varchar(20),  -- Telefone da Operadora
-     fax                      varchar(20),  -- Fax da Operadora
-     endereco_eletronico      varchar(255),  -- E-mail da Operadora
-     representante            varchar(50),  -- Representante legal da Operadora
-     cargo_representante      varchar(40),  -- Cargo do representante legal
+     fax                      varchar(20) collate "pt_br_ci_ai" ,  -- Fax da Operadora
+     endereco_eletronico      varchar(255) collate "pt_br_ci_ai" ,  -- E-mail da Operadora
+     representante            varchar(50) collate "pt_br_ci_ai" ,  -- Representante legal da Operadora
+     cargo_representante      varchar(40) collate "pt_br_ci_ai" ,  -- Cargo do representante legal
      regiao_de_comercializacao smallint,  -- Região de Comercialização (número de 1 dígito)
      data_registro_ans        date,  -- Data do registro na ANS (formato AAAA-MM-DD)
 
