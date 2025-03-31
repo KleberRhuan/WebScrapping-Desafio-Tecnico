0ae54851e32cb548bf2fe5a22fb346a7dbcfb71f
KleberRhuan <kleber_rhuan@hotmail.com>
2025-03-30 22:00:20 -0300

    feat: integrate logging system with SLF4J and Logback

--- src/main/java/com/kleberrhuan/intuitivecare/Main.java
+++ src/main/java/com/kleberrhuan/intuitivecare/Main.java
@@ -1,12 +1,22 @@
 package com.kleberrhuan.intuitivecare;
 
-import com.kleberrhuan.intuitivecare.util.ScannerHelper;
+import com.kleberrhuan.intuitivecare.cli.CliManager;
+import com.kleberrhuan.intuitivecare.util.helpers.ScannerHelper;
+import ch.qos.logback.classic.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Main {
+    private static final Logger LOGGER = (Logger) LoggerFactory.getLogger(Main.class);
+
     public static void main(String[] args) {
+        LOGGER.info("Iniciando aplicação IntuitiveCare");
         try (ScannerHelper scannerHelper = new ScannerHelper()) {
             CliManager cliManager = new CliManager(scannerHelper);
             cliManager.run();
+        } catch (Exception e) {
+            LOGGER.error("Erro durante a execução do programa: {}", e.getMessage(), e);
         }
+
+        LOGGER.info("Aplicação finalizada");
     }
 }
